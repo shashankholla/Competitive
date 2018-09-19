675a901fbf332d5127cf4e213f441530e29933f3
shashankholla <shashu1999@gmail.com>
2018-09-19 07:29:03 +0530

    Add files via upload

---
+++ RajandSlippers.py
@@ -0,0 +1,44 @@
+def isBetween(earliest,friendTime):
+    #Code to check if inbetween
+
+    return True
+
+
+
+
+    else return False
+
+
+
+if __name__ == "__main__":
+    testCases = int(input())
+
+    while(t):
+        friends = []
+        minBB = [0,0]
+        t = t-1
+        nFriends = int(input())
+        tIn = list(map(int,input().split(':'))
+        tUp = list(map(int,input().split(':'))
+        tOpen = list(map(int,input().split(':'))
+        earliest = [tOpen[0]+(R+tOpen[1])//60,(tOpen[1] + R)%60]
+        R = int(input())
+        S = int(input())
+        ttravel = S + 2*R
+        ttravel = [ttravel//60,ttravel%60]
+        while(nFriends):
+            inpt = input()
+            inpt = inpt.split(" ")
+            inpt[0] = inpt[0].split(":")
+            inpt[1] = inpt[1].split(":")
+            if(isBetween(earliest,inpt)):
+                
+
+
+            friends.append(inpt)
+
+
+    
+        
+        
+    
